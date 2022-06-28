4516dba01ad169ae28ef5f960266c2a742b01065
Joe <hutcheson2001@yahoo.com>
2022-06-28 11:41:07 -0400

    commit tree 23c9fea1e63b89f2e74f901a7fb1f541ff7ed9b0

--- src/features/books/Books.js
+++ src/features/books/Books.js
@@ -3,15 +3,15 @@ import { useSelector } from "react-redux";
 
 function Books() {
   const books = useSelector((state) => state.books);
-
+  console.log(books)
   return (
     <div>
       <ul>
-        {books.map((book) => (
+        {books ? books.map((book) => (
           <li key={book.id}>
             {book.title} by {book.authorName}
           </li>
-        ))}
+        )): ""}
       </ul>
     </div>
   );
